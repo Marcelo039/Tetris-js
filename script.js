e142d92f1deb10f51ce8a9bb3f3db460f4dc31b9
Marcelo Fragoso <96299186+Marcelo039@users.noreply.github.com>
2026-03-01 14:52:18 -0300

    Alteracoes no script.js

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
 const canvas = document.getElementById("game");
 const context = canvas.getContext("2d");
 
-context.fillStyle = "red"
-context.fillRect(50, 50, 50, 50);
+const ROWS = 20;
+const COLS = 10;
+const BLOCK_SIZE = 30;
+
+canvas.width = COLS * BLOCK_SIZE;
+canvas.height = ROWS * BLOCK_SIZE;
+
+function drawGrid() {
+    context.strokeStyle = "#333";
+
+    for (let row = 0; row < ROWS; row++){
+        for (let col = 0; col < COLS; col++){
+            context.strokeRect(
+                col * BLOCK_SIZE,
+                row * BLOCK_SIZE,
+                BLOCK_SIZE,
+                BLOCK_SIZE
+            );
+        }
+    }
+}
+drawGrid();
+
+let position = { x: 4, y: 0 };
+
+function drawBlock(x, y, color = "red") {
+  context.fillStyle = color;
+  context.fillRect(
+    x * BLOCK_SIZE,
+    y * BLOCK_SIZE,
+    BLOCK_SIZE,
+    BLOCK_SIZE
+  );
+}
+
+function update() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  drawGrid();
+  drawBlock(position.x, position.y);
+
+  position.y++;
+}
+
+setInterval(update, 500);
